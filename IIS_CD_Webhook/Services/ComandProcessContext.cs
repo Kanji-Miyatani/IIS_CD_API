8fb2e10d4eb68dcee1da7a109661d4c016e3f9dd
MWSERVER\k-miyatani <k-miyatani@mediaware.co.jp>
2023-02-22 11:16:20 +0900

    ログ出力を一旦破棄

--- IIS_CD_Webhook/Services/ComandProcessContext.cs
+++ IIS_CD_Webhook/Services/ComandProcessContext.cs
@@ -13,7 +13,6 @@ namespace IIS_CD_Webhook.Services
             p.StartInfo.FileName = Environment.GetEnvironmentVariable("ComSpec");
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardInput = true;
-            p.StartInfo.RedirectStandardOutput = true;
             p.Start();
             _p = p;
             _logger = logger;
@@ -35,8 +34,6 @@ namespace IIS_CD_Webhook.Services
         public void Dispose()
         {
             _p.StandardInput.Close();
-            var msg = _p.StandardOutput.ReadToEnd();
-            _logger.LogDebug(msg, ConsoleColor.Yellow);
             _p.Close();
             _p.Dispose();
         }
