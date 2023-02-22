f1bd976f3743ff8113c194c0c7cd77bc9173fb2c
MWSERVER\k-miyatani <k-miyatani@mediaware.co.jp>
2023-02-22 15:32:10 +0900

    テストコントローラ作成

---
+++ IIS_CD_Webhook/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IIS_CD_Webhook.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("connection success!");
+        }
+    }
+}
